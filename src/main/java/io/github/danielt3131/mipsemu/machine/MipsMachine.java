974706a94449f0497b8af2d785b5d62d72d77f7b
alexander garcia <alexthekidmg@gmail.com>
2024-02-29 11:28:40 -0500

    Redid registers as one array

--- src/main/java/io/github/danielt3131/mipsemu/machine/MipsMachine.java
+++ src/main/java/io/github/danielt3131/mipsemu/machine/MipsMachine.java
@@ -25,14 +25,10 @@ import java.util.Scanner;
 public class MipsMachine {
 
     //Register Variables
-    private final int zero = 0; //zero register $0
     private int hi, lo; //high and low of multiplication and division
     private int pc; //program counter
-    private int t0, t1, t2, t3, t4, t5, t6, t7, t8, t9; //temporary registers $8-$15, $24,$25
-    private int a0, a1, a2, a3; //argument registers $4-$7
-    private int v0, v1; //result registers $2, $3
-    private int s0, s1, s2, s3, s4, s5, s6, s7; //saved registers $16-$23
-    private int at, k0, k1, gp, fp, sp, ra; //specific-purpose registers $1, $26-$31
+
+    private int[] register = new int[32];
 
     //Machine memory
     private Word[] memory;
